0adc124dda4d88cf093d976fc4d671fdddcd9c3d
Ville M. Vainio <ville.vainio@basware.com>
2018-07-23 18:15:44 +0300

    switch to wp --watch

--- app.ts
+++ app.ts
@@ -1,11 +1,4 @@
-
-// the import here
-//import "./app.scss";
-// import ends
-
 import {MDCTextField} from '@material/textfield';
 
 const username = new MDCTextField(document.querySelector('.username'));
 const password = new MDCTextField(document.querySelector('.password'));
-
-console.log('hello')
