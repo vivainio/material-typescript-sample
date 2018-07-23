45b7617e6280e64b2ff37f6779ee66d359cf9e48
Ville M. Vainio <ville.vainio@basware.com>
2018-07-23 17:31:28 +0300

    styles as loaded from scss work

--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
-console.log('hello world')
+
+// the import here
+import "./app.scss";
+// import ends
+
+import {MDCTextField} from '@material/textfield';
+
+const username = new MDCTextField(document.querySelector('.username'));
+const password = new MDCTextField(document.querySelector('.password'));
+
+console.log('new version')
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,5 @@
 module.exports = [
   {
-    entry: './app.scss',
-    mode: "development",
-    output: {
-      // This is necessary for webpack to compile
-      // But we never use style-bundle.js
-      filename: 'style-bundle.js',
-    },
-    module: {
-      rules: [{
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'bundle.css',
-            },
-          },
-          { loader: 'css-loader' },
-          {
-            loader: 'sass-loader',
-            options: {
-              includePaths: ['./node_modules'],
-            }
-          },
-        ]
-      }]
-    },
-  },
-  {
     entry: "./app.ts",
     mode: "development",
     devtool: "inline-source-map",
@@ -39,8 +10,23 @@ module.exports = [
     module: {
       rules: [
         // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-        { test: /\.tsx?$/, loader: "ts-loader" }
+        { test: /\.tsx?$/, loader: "ts-loader" },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: "style-loader"
+            },
+            { loader: "css-loader" },
+            {
+              loader: "sass-loader",
+              options: {
+                includePaths: ["./node_modules"]
+              }
+            }
+          ]
+        }
       ]
-    },
+    }
   }
 ];
